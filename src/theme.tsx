45ea60eeae22b5a507b0b56e17bee8482ed30711
Henrique Bruno <henrique.bruno.fa@gmail.com>
2022-02-06 15:46:50 -0300

    0.2.3

--- src/theme.tsx
+++ src/theme.tsx
@@ -3,10 +3,15 @@ import deepmerge from 'deepmerge';
 // import { useColorScheme } from 'react-native';
 
 
-
+/** Prettyfies object type */
 type Id<T> = unknown & { [P in keyof T]: T[P] };
+// https://stackoverflow.com/a/61132308/10247962
+type DeepPartial<T> = T extends object ? {
+  [P in keyof T]?: DeepPartial<T[P]>;
+} : T;
 
 
+// Some were based on https://callstack.github.io/react-native-paper/theming.html
 type Colors = {
   primary: string;
   // secondary: string;
@@ -25,6 +30,7 @@ type Colors = {
   header: string;
   _button: {
     text: string;
+    // TODO: /** Defaults to primary */
     action: string;
     neutral: string;
     destructive: string;
@@ -48,36 +54,16 @@ const defaultCommon = {
 type Common = typeof defaultCommon;
 
 
-// const defaultPresets = {
-//   screenBasic: {
-//     flex: 1,
-//     paddingVertical: 40,
-//     paddingHorizontal: 32,
-//   },
-//   screenBasicShort: {
-//     flex: 1,
-//     paddingVertical: 20,
-//     paddingHorizontal: 16,
-//   }
-// }
 
-// Some were based on https://callstack.github.io/react-native-paper/theming.html
+
 type Theme = {
   common: Common;
   colors: Colors;
   // presets: Presets;
 };
 
-/** Used in createThemes to set everything up. */
-type ThemesParam = {
-  /** @default 'light' */
-  initialTheme: string;
-  /** Roundness of the button, at least. */
-  themes: Record<string, Theme>;
-};
 
 
-// type Transforme
 type ThemeReturn = Id<
   Theme & {
     $settings: {
@@ -91,6 +77,7 @@ type ThemeReturn = Id<
 >;
 
 
+const defaultInitialTheme = 'light';
 const defaultLightTheme: Theme = {
   common: defaultCommon,
   colors: {
@@ -110,29 +97,29 @@ const defaultLightTheme: Theme = {
   },
 };
 
-const defaultInitialTheme = 'light';
-const defaultThemeContextValue: ThemesParam = {
-  initialTheme: defaultInitialTheme,
-  themes: {
-    light: defaultLightTheme,
-  },
-};
 
 
 
-function createUseThemeData({ themesParam, themeId }: {themesParam: ThemesParam; themeId?: string}): ThemeReturn {
+
+/** Selects the theme */
+function createUseThemeData({ themes, themeId, initialTheme }: {themes: DeepPartialThemes; themeId?: string; initialTheme: string}): ThemeReturn {
   const theme: Theme = deepmerge.all([
-    themesParam.themes[defaultInitialTheme] ?? {},
-    themesParam.themes[themesParam.initialTheme] ?? {},
-    ...(themeId ? [themesParam.themes[themeId] ?? {}] : []),
+    defaultLightTheme,
+    themes[defaultInitialTheme] ?? {},
+    themes[initialTheme] ?? {},
+    ...(themeId ? [themes[themeId] ?? {}] : []),
   ]) as Theme;
+
+  function changeTheme(themeId: string) {
+    setGlobalState('useThemeData', createUseThemeData({ themes, themeId, initialTheme }));
+  }
 
   return {
     ...theme,
     $settings: {
-      availableThemes: Object.keys(themesParam.themes),
-      currentTheme: themeId ?? themesParam.initialTheme, // initialTheme defaults to defaultInitialTheme.
-      changeTheme: (themeId) => setGlobalState('theme', () => createUseThemeData({ themesParam, themeId })),
+      availableThemes: Object.keys(themes),
+      currentTheme: themeId ?? initialTheme, // initialTheme defaults to defaultInitialTheme.
+      changeTheme,
     },
   };
 }
@@ -140,26 +127,24 @@ function createUseThemeData({ themesParam, themeId }: {themesParam: ThemesParam;
 
 
 const { useGlobalState, setGlobalState } = createGlobalState<{
-  params: ThemesParam;
-  theme: ThemeReturn;
+  useThemeData: ThemeReturn;
 }>({
-  params: defaultThemeContextValue,
-  theme: createUseThemeData({ themesParam: defaultThemeContextValue, themeId: defaultInitialTheme }),
+  useThemeData: createUseThemeData({
+    themes: { light: defaultLightTheme },
+    initialTheme: defaultInitialTheme,
+  }),
 });
 
 
 /** To be used only inside react-native-gev. To use in your app, use createThemes(). */
 export function useTheme(): ThemeReturn {
-  return useGlobalState('theme')[0];
+  return useGlobalState('useThemeData')[0]; // [0] is the state. [1] is the setGlobalState.
 }
 
-// https://stackoverflow.com/a/61132308/10247962
-type DeepPartial<T> = T extends object ? {
-  [P in keyof T]?: DeepPartial<T[P]>;
-} : T;
 
 
-type CreateThemePops = {
+
+type DeepPartialThemes = {
   light?: DeepPartial<Theme>;
   dark?: DeepPartial<Theme>;
   // [others: string]: DeepPartial<Theme>,
@@ -170,8 +155,7 @@ type CreateThemeRtn = {
 };
 
 type CreateThemeOptions = {
-  noOptionsYet: null;
-
+  initialTheme?: string;
 };
 
 /** Changes the default colors of react-native-gev components.
@@ -179,8 +163,11 @@ type CreateThemeOptions = {
  * You may also use the given colors in your app by using useTheme() hook.
  *
  * As it uses `react-hooks-global-state`, it doesn't need a context provider. */
-export function createTheme<T extends CreateThemePops>(t: T, opts?: CreateThemeOptions): CreateThemeRtn {
-  createUseThemeData;
+export function createTheme<T extends DeepPartialThemes>(themes: T, opts?: CreateThemeOptions): CreateThemeRtn {
+  createUseThemeData({
+    themes,
+    initialTheme: opts?.initialTheme ?? defaultInitialTheme,
+  });
   return {
     useTheme,
   };
